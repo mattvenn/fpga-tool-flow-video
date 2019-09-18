8b35ad446972565490c2c3b4dc0067dc8bdd1add
matt venn <matt@mattvenn.net>
2019-09-18 17:39:26 +0200

    support lots of boards

--- rtl/simple.v
+++ rtl/simple.v
@@ -1,24 +1,9 @@
 module top (
-    input CLK,
     input BTN1,
     input BTN2,
     output LED1,
-    output LED2,
-    output LED3,
-    output LED4,
-    output LED5,
 );
-    assign LED1 = BTN1 & BTN2;
 
-    /*
-    assign {LED1,LED2,LED3,LED4,LED5} = register[22:18];
-    reg [22:0] register;
-    
-    always @(posedge CLK) begin
-        if(BTN1)
-            register <= register + 1;
-    end
-
-    */
+    assign LED1 = BTN1 | BTN2;
 
 endmodule
